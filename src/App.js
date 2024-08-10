e9d96c19aacd5aa4b3dca3f4351cd36fda089419
Bruno Alves <boteistem@gmail.com>
2024-08-09 23:04:59 -0300

    Arch of project

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import { Routes, Route } from 'react-router-dom';
+import RouteConfig from './config/Routes';
+const App = () => {
+   return RouteConfig();
+};
+ 
+export default App;
---
+++ src/config/Routes.js
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+const RouteConfig = () => {
+    return (
+        /*
+       <>
+          <Routes>
+             <Route path="/" element={<Home />} />
+          </Routes>
+       </>
+       */
+      ''
+    );
+ };
+  
+ export default RouteConfig;
